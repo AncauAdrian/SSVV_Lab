bb416dec40dfab29342043915fb78a111d06efca
Tudor Badiu <btie2464@scs.ubbcluj.ro>
2021-04-06 00:42:14 +0300

    oops

--- src/main/java/repository/AbstractXMLRepository.java
+++ src/main/java/repository/AbstractXMLRepository.java
@@ -110,7 +110,9 @@ public abstract class AbstractXMLRepository<ID, E extends HasID<ID>> extends Abs
             transformer.transform(new DOMSource(document),
                     new StreamResult(this.filename));
 
-
+        }catch(Exception e){
+        e.printStackTrace();
+        }
     }
 
     /**
